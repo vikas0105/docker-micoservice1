3d701c1ab6b5da4468d463f8795417915559f5d1
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2024-01-03 13:13:28 +0530

    Delete order-service directory

--- order-service/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const OrderSchema = new mongoose.Schema({
-  product: String,
-  quantity: Number,
-});
-
-module.exports = mongoose.model('Order', OrderSchema);
-
