ce5fc9be9f996d59cd6f008abaf0e130caddf9cf
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2024-01-03 13:14:45 +0530

    Delete product-service directory

--- product-service/models/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
-
